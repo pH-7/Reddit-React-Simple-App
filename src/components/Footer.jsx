4acd851c425779715b2b0bcc9eae55cbfb31a710
Pierre-Henry Soria <hi@ph7.me>
2022-10-09 17:25:31 +1030

    Update footer message (with my homepage URL)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <p>Done by Pierre-Henry</p>
+      <p>
+        Done with passion by <a href="https://ph7.me">Pierre-Henry</a>
+      </p>
     </>
   );
 };
